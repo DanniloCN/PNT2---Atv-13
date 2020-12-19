c79ef07edf25bacf6b93caae9ebc8d26ef52eb8d
DanniloCN <dannilocarlos@gmail.com>
2020-12-19 14:37:07 -0300

    4 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  @ViewChild(Nav) nav:Nav;
+  rootPage:any = HomePage;
+  pages: Array<(title: string, component: any)>;
 
   constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+    platform.ready().then(() => {});
 
-    });
+    this.pages = [
+      (title: 'Page1', component: HomePage)
+      (title: 'Page2', component: AboutPage)
+    ];
+  }
+
+  openPage(page) {
+    this.nav.setRoot(page.component);
   }
 }
